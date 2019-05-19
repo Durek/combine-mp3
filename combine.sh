216890e91b5644211d606bf0a310a3f7203c1609
M0V3 <m0v3@m0v3.de>
2019-05-19 19:39:12 +0200

    Update combine.sh

--- combine.sh
+++ combine.sh
@@ -14,4 +14,5 @@ _GENRE="Fantasy"
 mp3wrap output.mp3 *.mp3
 id3v2 output_MP3WRAP.mp3 -y "$_YEAR" -A "$_ALBUM" -a "$_ARTIST" -t "$_TITLE" -g "$_GENRE"
 mp3val -f output_MP3WRAP.mp3
-mv output_MP3WRAP.mp3.bak output_MP3WRAP_unfixed.mp3
+mv output_MP3WRAP.mp3.bak output_unfixed.mp3
+mv output_MP3WRAP.mp3 output.mp3
