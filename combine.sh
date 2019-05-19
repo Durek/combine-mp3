53247e41685be07c3ab5ab4ff7ca0b9e6535c265
M0V3 <m0v3@m0v3.de>
2019-05-19 19:38:03 +0200

    Create combine.sh

---
+++ combine.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+### EDIT THESE VARIABLES ###
+
+# Uncomment to make script work
+#UNCOMMENT_WHEN_READY="duh"
+
+# MP3 TAGS
+_YEAR="2004"
+_ALBUM="Die Zwerge"
+_ARTIST="Markus Heitz"
+_TITLE="Der Krieg der Zwerge"
+_GENRE="Fantasy"
+
+############################
+
+mp3wrap output.mp3 *.mp3
+id3v2 output_MP3WRAP.mp3 -y "$_YEAR" -A "$_ALBUM" -a "$_ARTIST" -t "$_TITLE" -g "$_GENRE"
+mp3val -f output_MP3WRAP.mp3
+mv output_MP3WRAP.mp3.bak output_MP3WRAP_unfixed.mp3
