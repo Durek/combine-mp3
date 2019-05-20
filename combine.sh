5ebb74543c9c7ef2340538402cb9994dcd4cbad3
M0V3 <m0v3@m0v3.de>
2019-05-20 03:52:47 +0200

    Update combine.sh

--- combine.sh
+++ combine.sh
@@ -11,6 +11,7 @@ _ALBUM="Die Zwerge"
 _ARTIST="Markus Heitz"
 _TITLE="Der Krieg der Zwerge"
 _GENRE="Fantasy"
+_TRACKNUM="1"
 
 ############################
 
@@ -21,7 +22,7 @@ if [ -z ${UNCOMMENT_WHEN_READY+x} ]; then
 fi
 
 mp3wrap output.mp3 *.mp3
-id3v2 output_MP3WRAP.mp3 -y "$_YEAR" -A "$_ALBUM" -a "$_ARTIST" -t "$_TITLE" -g "$_GENRE"
+id3v2 output_MP3WRAP.mp3 -y "$_YEAR" -A "$_ALBUM" -a "$_ARTIST" -t "$_TITLE" -g "$_GENRE" -T "$_TRACKNUM"
 mp3val -f output_MP3WRAP.mp3
 mv output_MP3WRAP.mp3.bak output_unfixed.mp3
 mv output_MP3WRAP.mp3 output.mp3
